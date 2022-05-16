8efb290d2d3ab9d084d7c5043af89b9f512a37de
Zauri Metreveli <72525534+Metrevelii@users.noreply.github.com>
2022-05-16 17:53:43 +0400

    finished section 8. Error popup managing

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -10,14 +10,24 @@ const AddUser = (props) => {
   const [enteredUsername, setEnteredUsername] = useState("");
   const [enteredAge, setEnteredAge] = useState("");
 
+  const [error, setError] = useState();
+
   const addUserHandler = (event) => {
     event.preventDefault();
     // return statement finishes the function execution. code after that wont be executed if length === 0;
 
     if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      setError({
+        title: "Invalid input",
+        message: "Please enter a valid name and age (non-empty values).",
+      });
       return;
     }
     if (+enteredAge < 1) {
+      setError({
+        title: "Invalid age",
+        message: "Please enter a valid age (> 0).",
+      });
       return;
     }
 
@@ -33,9 +43,21 @@ const AddUser = (props) => {
     setEnteredAge(event.target.value);
   };
 
+  // Removing error popup
+  const errorHandler = () => {
+    setError(null);
+  };
+
   return (
     <div>
-      <ErrorModal title="An error occured!" message="Something went wrong!" />
+      {/* Error modal will only be output if we have an error */}
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onConfirm={errorHandler}
+        />
+      )}
       <Card className={styles.input}>
         <form onSubmit={addUserHandler}>
           <label htmlFor="username">Username</label>
